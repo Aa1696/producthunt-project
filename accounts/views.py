bd6fcc7df2b0f9b72616d88b4c58a68f316d6086
Aa1696 <sonumahtoraj121@gmail.com>
2022-06-04 19:00:28 +0530

    Authenticate the password and user

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 # Create your views here.
 def login(request):
     return render(request,'account/login.html')
 def logout(request):
     return render(request,'account/logout.html')
 def signup(request):
-    return render(request,'account/signup.html')
+    if(request.method=='POST'):
+        if(request.POST['new_psswd']==request.POST['cnf_psswd']):
+            try:
+               user=User.objects.get(username=request.POST['username'])
+               return render(request,'account/signup.html',{'error':'User already enrolled!'})
+            except User.DoesNotExist:
+                user=User.objects.create_user(username=request.POST['username'],password=User.objects.get(username=request.POST['new_psswd']))
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request,'account/signup.html',{'error':'Password must match!'})
+
+
+    else:
+        return render(request,'account/signup.html')
+
