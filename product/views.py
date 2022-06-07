0920dac91419ddabba9e38b563906dcbf94f327b
Aa1696 <sonumahtoraj121@gmail.com>
2022-06-07 22:36:58 +0530

    Made changes in detail and home and completed the project

--- product/views.py
+++ product/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
 # Create your views here.
 def home(request):
-    return render(request,'products/home.html')
+    products=Product.objects
+    return render(request,'products/home.html',{'products':products})
 @login_required
 def create(request):
     if(request.method=='POST'):
@@ -22,7 +23,7 @@ def create(request):
             product.hunter=request.user
             product.votes_total=1
             product.save()
-            return redirect('home')
+            return redirect('/product/' + str(product.id))
 
 
         else:
@@ -30,5 +31,15 @@ def create(request):
     else:
         return render(request,'products/create.html')
 
+def detail(request,product_id):
+    product=get_object_or_404(Product,pk=product_id)
+    return render(request,'products/detail.html',{'product':product})
 
+@login_required
+def upvote(request,product_id):
+    if(request.method=='POST'):
+        product=get_object_or_404(Product,pk=product_id)
+        product.votes_total +=1
+        product.save()
+        return redirect('/product/' + str(product.id))
 
